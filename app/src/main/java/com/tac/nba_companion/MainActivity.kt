49a3493445ffcf768d307847ca66498708919984
Axel Labarre <axel.labarre@gmail.com>
2023-11-29 16:45:07 +0100

    (TopAppBar) add topAppBar in MainActivity

--- app/src/main/java/com/tac/nba_companion/MainActivity.kt
+++ app/src/main/java/com/tac/nba_companion/MainActivity.kt
@@ -1,19 +1,28 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.tac.nba_companion
 
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tac.nba_companion.ui.theme.NbaCompanionTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,8 +33,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
-
+                    Scaffold(
+                        topBar = { mTopAppBar() },
+                        content = { paddingValues ->  Box(modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)) },
+                    )
                 }
             }
         }
@@ -46,8 +59,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun mTopAppBar() {
+        TopAppBar(
+            colors = TopAppBarDefaults.smallTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            title = {
+                Text("NBA Companion")
+            }
+        )
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
